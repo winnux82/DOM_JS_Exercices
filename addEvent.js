6536e5b8197a26c2dccf3ab263cf658f8eca1a76
WnX <vandermeulen.christophe@gmail.com>
2022-12-01 09:30:51 +0100

    gitignore

---
+++ addEvent.js
@@ -0,0 +1,23 @@
+let a = document.querySelector('a');
+let button = document.querySelector('button');
+
+a.addEventListener('click', (e) => {
+    if (confirm('Etes-vous sûr ?')) {
+        location.href = "https://believemy.com";
+    }
+});
+
+button.addEventListener('mouseover', () => {
+    document.body.style.backgroundColor = 'orange';
+});
+
+function backgroundWhite() {
+    document.body.style.backgroundColor = 'white';
+}
+
+button.addEventListener('mouseout', backgroundWhite);
+button.addEventListener('mouseout', () => {
+    document.body.style.fontFamily = 'arial';
+});
+
+button.removeEventListener('mouseout', backgroundWhite);
